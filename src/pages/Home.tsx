042d9319337a9f639ec5a26a63728d278611c200
Jason Green <jgreen44@asu.edu>
2023-02-23 00:26:18 -0600

    refresh button added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,16 @@ import React from 'react';
 import { Tokens, Transactions } from '../components';
 import { Pools } from '../components/Pools';
 
+const handleRefresh = () => {
+  window.location.reload();
+};
 export const Home = () => {
   return (
     <>
       <div className={'d-flex gap-3 mb-4'}>
-        <button className={'btn btn-primary'}>Add Liquidity</button>
+        <button className={'btn btn-primary'} onClick={handleRefresh}>
+          Click to Reset
+        </button>
       </div>
       <Pools />
       <hr />
